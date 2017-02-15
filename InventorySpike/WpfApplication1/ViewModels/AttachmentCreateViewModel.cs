33e905d975d2613008943ce89d47ce56c2a102eb
David Su <dsu@digitalglobe.com>
2017-02-14 21:54:23 -0500

    Attachment - part 2

---
+++ InventorySpike/WpfApplication1/ViewModels/AttachmentCreateViewModel.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+using Caliburn.Micro;
+using Client.Classes;
+using Client.Framework;
+using Client.Properties;
+using Client.ViewModels.Messages;
+using Inventory.Business;
+using Inventory.Business.Services;
+using Microsoft.Practices.ServiceLocation;
+using PropertyChanged;
+using Action = System.Action;
+using ILog = log4net.ILog;
+using LogManager = log4net.LogManager;
+
+namespace Client.ViewModels
+{
+    [ImplementPropertyChanged]
+    public class AttachmentCreateViewModel : ViewModelBase, ISaveCancelDialog
+    {
+        private static ILog logger = LogManager.GetLogger(typeof(AttachmentCreateViewModel));
+
+        private readonly IInvWindowManager _windowManager;
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainViewModel"/> class.
+        /// </summary>
+        protected AttachmentCreateViewModel()
+        {
+            Init();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the MainWindowViewModel class.
+        /// </summary>
+        public AttachmentCreateViewModel(FacilityAttachmentViewModel attachment,
+                        IEventAggregator eventAggregator,
+                        IInvWindowManager windowManager
+                        )
+            : base(eventAggregator)
+        {
+            Attachment = attachment;
+            _windowManager = windowManager;
+
+            this.SubscribeToEvents();
+
+            Init();
+        }
+
+        private void Init()
+        {
+        }
+
+        #endregion
+
+        #region Properties
+
+        public FacilityAttachmentViewModel Attachment { get; set; }
+
+        #endregion
+
+        #region Public Methods
+
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+
+        #region ISaveCancelDialog
+
+        public bool CanSave
+        {
+            get
+            {
+                // return !IsProcessing && this.FishCatch != null && this.FishCatch.HasChanges;
+                return true;
+            }
+        }
+
+        public bool CanCancel
+        {
+            get { return true; }
+        }
+
+        public void Save()
+        {
+            // validation
+
+            //CursorHelper.ExecuteWithWaitCursor(() =>
+            //{
+            //    this.Facility.SaveFacility(true,
+            //delegate (InvFacility facility)
+            //{
+            //    this.Facility.Model = facility;
+            //    _windowManager.Inform("Create Facility", "Facility saved successfully");
+            //    this.DialogResult = true;
+
+            //    EventAggregator.PublishOnUIThread(new FacilityUpdatedMessage()
+            //    {
+            //        FacilityUpdateType = FacilityUpdateType.Create,
+            //        Facility = facility,
+            //    });
+            //},
+            //delegate
+            //{
+            //    _windowManager.ShowError("Create Attachment", "Facility save failed");
+            //});
+        }
+
+        public void Cancel()
+        {
+            this.DialogResult = false;
+        }
+
+        public bool? DialogResult { get; private set; }
+
+        #endregion
+    }
+}
---
+++ InventorySpike/WpfApplication1/ViewModels/AttachmentInfoViewModel.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+using Caliburn.Micro;
+using Client.Classes;
+using Client.Framework;
+using Client.Properties;
+using Inventory.Business;
+using Inventory.Business.Services;
+using Microsoft.Practices.ServiceLocation;
+using PropertyChanged;
+using Action = System.Action;
+using ILog = log4net.ILog;
+using LogManager = log4net.LogManager;
+
+namespace Client.ViewModels
+{
+    [ImplementPropertyChanged]
+    public class AttachmentInfoViewModel : EntityViewModel<InvFacilityAttachment>
+    {
+        private static ILog logger = LogManager.GetLogger(typeof(AttachmentInfoViewModel));
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainViewModel"/> class.
+        /// </summary>
+        protected AttachmentInfoViewModel()
+        {
+            Init();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the MainWindowViewModel class.
+        /// </summary>
+        public AttachmentInfoViewModel(InvFacilityAttachment facilityAttachment,
+                        IApplicationContext applicationContext,
+                        IEventAggregator eventAggregator)
+            : base(facilityAttachment, eventAggregator)
+        {
+            ApplicationContext = applicationContext;
+            this.SubscribeToEvents();
+
+            Init();
+        }
+
+        private void Init()
+        {
+        }
+
+        #endregion
+
+        #region Properties
+
+        public IApplicationContext ApplicationContext { get; private set; }
+
+        //public IEnumerable<string> FacilitySystems
+        //{
+        //    get
+        //    {
+        //        switch (this.Model.FacilityGroup)
+        //        {
+        //            case "Electrical System":
+        //                yield return "Emergency Light";
+        //                yield return "Test Elec Sys Sys";
+        //                break;
+        //            case "Mechanical System":
+        //                yield return "Air Handling Unit";
+        //                yield return "Test Mechanical Sys Sys";
+        //                break;
+        //            case "Electrical Equipment":
+        //                yield return "Control Panel";
+        //                yield return "Test Elec Sys Equip";
+        //                break;
+        //            case "Mechanical Equipment":
+        //                yield return "Air Dryer";
+        //                break;
+        //            default:
+        //                break;
+        //        }
+        //    }
+        //}
+
+        #endregion
+
+        #region Public Methods
+
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+    }
+}
